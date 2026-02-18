14ecdbc5db5e6d6aa907824e83b0f6289a645c3b
Funsho Okubanjo <signups@funsho.me>
2026-02-18 08:37:43 +0100

    Update AWS credentials

--- src/common/utils/secrets.util.ts
+++ src/common/utils/secrets.util.ts
@@ -7,8 +7,8 @@ import { SafeLogger } from './logger.util';
 export class SecretsUtil {
   private static client = new SecretsManagerClient({
     credentials: {
-      accessKeyId: process.env.APP_AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.APP_AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.APP_AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.APP_AWS_SECRET_ACCESS_KEY!,
     },
     region: process.env.AWS_REGION || 'us-east-1',
   });
